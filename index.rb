34c4bfd6cdcf9514aa663080292b4763959b5e3c
takumi-shibata <241448ta@gmail.com>
2020-09-26 00:28:45 +0900

    2周目 学習終了

--- index.rb
+++ index.rb
@@ -195,3 +195,117 @@ menu1 = Menu.new(name:"すし",price:1000)
 
 puts menu1.info
 # →すし 1000円
+
+
+
+# 8.ファイルの分割(分割先のファイルで定義されているクラスを使用するにはrequireする必要がある。jsと似ている、詳しくはjs5のリポジトリ参照)
+# 使用するファイルの先頭で「require "./ファイル名"」もしくは「require "../フォルダー名/ファイル名"」と記述する。 ※ファイル名の拡張子の「.rb」は省略することができる
+require "./menu"
+
+menu1 = Menu.new(name: "すし", price: 1000)
+puts menu1.info
+# →すし 1000円
+
+
+
+# 9.インスタンスの配列(クラスから生成したインスタンスも、配列の要素にすることが可能)
+require "./menu"
+
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
+
+menus =[menu1,menu2,menu3,menu4]
+
+menus.each do |menu|
+  puts menu.info
+end
+# →ピザ 800円 すし 1000円 コーラ 300円　お茶 200円
+
+
+# ↓※項目に数字の番号の表示↓
+require "./menu"
+
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
+
+menus = [menu1, menu2, menu3, menu4]
+
+# 変数indexを定義し、項目に数字の番号の表示
+index = 0
+
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  
+  # 数字の番号が入った変数indexにeachで繰り返し1を足していく
+  index += 1
+end
+# →0.ピザ 800円 1.すし 1000円 2.コーラ 300円　3.お茶 200円
+
+
+
+# 10.入力の受け取り(文字列や数字をコンソールで入力し、その入力した値を変数に代入する事ができる。jsではパッケージ、詳しくはjs5のリポジトリ参照)
+# 文字列は→「変数名 = gets.chomp  数字は→「変数名 = gets.chomp.to_i」
+puts "名前を入力してください"
+
+# ↓文字列の入力の受け取り
+name = gets.chomp
+
+# ↓受け取った結果
+puts "#{name}さん、いらっしゃいませ"
+
+puts "1つ100円のお菓子があります"
+puts "いくつ買いますか?"
+
+# ↓数字の入力の受け取り
+count = gets.chomp.to_i
+
+# 合計金額の定義
+total_price = 100 * count
+
+# ↓受け取った結果
+puts "お会計は#{total_price}円です"
+# →名前を入力してください (文字列をコンソールで入力) (文字列)さん、いらっしゃいませ 1つ100円のお菓子があります いくつ買いますか? (数字をコンソールで入力)　お会計は(合計金額)円です
+
+
+
+# ↓全ての内容のまとめ↓
+require "./menu"
+
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
+
+menus = [menu1, menu2, menu3, menu4]
+
+# ↓メニュー番号の定義
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+
+  # メニュー番号が入った変数indexにeachで繰り返し1を足していく
+  index += 1
+end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+
+# メニュー番号(数字)の受け取り
+order = gets.chomp.to_i
+
+# ↓menus[order(上記で受け取ったメニュー番号)]は、menus[インデックス番号]って事。それをselected_menuに代入しているので、selected_menuはmenus[インデックス番号]と同じ
+selected_menu = menus[order]
+
+# ↓メニュー名の表示
+puts "選択されたメニュー: #{selected_menu.name}"
+puts "個数を入力してください(3つ以上で100円割引)"
+
+# ↓購入する個数(数字)の受け取り
+count = gets.chomp.to_i
+
+# ↓合計金額の表示(先程選択したselected_menuに入っているメニュー名と金額。及び、countに入っている購入する個数をget_total_priceメソッドに値の受け渡し)
+puts "お会計は#{selected_menu.get_total_price(count)}円です”
